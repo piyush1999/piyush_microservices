56e0cebe180ed5569dc7e973c171b5ac455fe0ec
piyush likhitkar <piyushlikhitkar1999@gmail.com>
2023-03-19 15:19:19 +0530

    Initial commit

---
+++ customer/src/main/java/com/piyushcode/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.piyushcode.customer;
+
+public record CustomerRegistrationRequest(String firstName,String lastName,String email) {
+}
---
+++ customer/src/main/java/com/piyushcode/customer/CustomerService.java
@@ -0,0 +1,12 @@
+package com.piyushcode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer= Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
+
+    }
+}
